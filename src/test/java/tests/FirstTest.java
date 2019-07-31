0d2b574cb617f1a342d49cb83c8ef9bd79a9d3da
obogush <ozlembogush@gmail.com>
2019-07-30 20:38:46 -0500

    Try

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,4 +1,16 @@
 package tests;
 
 public class FirstTest {
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
 }
+/*
+
+
+git fetch origin master:tmp
+git rebase tmp
+git push origin HEAD:master
+git branch -D tmp
+ */
---
+++ src/test/java/tests/SecondTest.java
@@ -0,0 +1,7 @@
+package tests;
+
+public class SecondTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
